9b44ab9ca9206eef458e09c89db54b30b5db50f3
Alberto Ortega Padilla <albertoortega576@gmail.com>
2016-10-07 10:48:49 +0200

    Actualizo EJERCICIOS

--- 8_coches/JS/script.js
+++ 8_coches/JS/script.js
@@ -6,18 +6,21 @@ var cocheApostado;
 var numeroCoche;
 var coches = [];
 var clasificacion = [];
+//FUNCION GENERAR LOS COCHES: 6 COCHES Y  OTRAS COSAS.
 function generarCoches(){
   document.getElementById("go").disabled = true;
+  //AQUI CONCRETAMENTE CREAMOS H3 PARA EL SALDO DEL JUGADOR IMPRIMIRLO.
   var saldo = document.createElement("h3");
   saldo.id="tituloD";
   document.getElementById("circuito").appendChild(saldo);
   document.getElementById("tituloD").innerHTML = "<strong>Saldo: </strong> " + dinero + "€";
   document.getElementById("tituloD").style.visibility="visible";
-  //aqui hacemos visibles e invisibles los inpunt, botonoes, etc...
+  //AQUI HACEMOS VISIBLES E INVISIBLES LOS INPUT, DIV ETC.
   document.getElementById("apuesta").style.visibility="visible";
   document.getElementById("start").style.visibility="hidden";document.getElementById("go").style.visibility="visible";
   document.getElementById("nCoche").style.visibility="visible";document.getElementById("apuesta1").style.visibility="visible";
   document.getElementById("ha").style.visibility="visible";document.getElementById("hb").style.visibility="visible";
+  //GENERAMOS LOS 6 COCHES.
   for (var i=0;i<6;i++){
     var coche = document.createElement("img");//crea variable de la Imagen coche+ numero
     coche.id="coche" + (i + 1);//crea la clase id del coche
@@ -32,6 +35,7 @@ function generarCoches(){
   lineaSalidaMeta();
   cochesEnApuesta();
 }
+//GENERAMOS LAS LINEAS DE SALIDAS Y LAS BANDERAS DE SALIDA Y LLEGADA.
 function lineaSalidaMeta() {
   posicion = 0;
   numero = 1;
@@ -56,7 +60,28 @@ function lineaSalidaMeta() {
     posicion = 900;
   }
 }
-//function go() es para los poner invisibles y crear la variable coche = a los coches id.
+//ESTO ES PARA LA OPCION DE APUESTA, LA ELECCION DEL JUGADOR DE QUE COCHE VA A APOSTAR.
+function cochesEnApuesta() {
+    document.getElementById("coches").innerHTML = "";
+    for(var i = 1; i <= 6; i++) {
+        document.getElementById("coches").innerHTML += "<option value=\"" + i + "\">" + i + "</option>";
+    }
+}
+//AQUI ESTA ABARCA EL APARTADO DE APUESTA.
+function apuesta(){
+  document.getElementById("go").disabled = false;
+  document.getElementById("apuesta").style.visibility="hidden";
+  if (dinero >= parseInt(document.getElementById("apuesta1").value)) {
+      dinero -= parseInt(document.getElementById("apuesta1").value);
+      cocheApostado = "coche" + document.getElementById("coches").value;
+  }else if (dinero <= parseInt(document.getElementById("apuesta1").value)) {
+      document.getElementById("dinero").innerHTML = "<strong>NO PUEDES APOSTAR MAS QUE TU SALDO:" + dinero
+      + "€ Y HAS APOSTADO: " + document.getElementById("apuesta1").value + "€</strong>";
+  }else {
+      document.getElementById("dinero").innerHTML = "<strong>SALDO: </strong> " + dinero + "€";
+  }
+}
+//HACER INVISIBLES ALGUNOS ELEMENTOS Y CREAR VARIABLE COCHE, AÑADIR ID Y EJECUTAR RESPECTIVAMENTE FUNCION CORRER.
 function go(){
   document.getElementById("go").style.visibility="hidden";
   document.getElementById("ha").style.visibility="hidden";document.getElementById("hb").style.visibility="hidden";
@@ -66,7 +91,7 @@ function go(){
       correr(document.getElementById(coche));
   }
 }
-//se mueven los coches
+//SE MUEVEN LOS COCHES Y ADEMAS AÑADE LAS CLASIFICACION, PERO NO LOS IMPRIME, ESO LO HACE LA FUNCION mostrarClasificacion.
 function correr(coche){
   document.getElementById("tituloD").style.visibility="hidden";
   //coche es la variable enviada en la function go() que envia todos los id de coches.
@@ -90,9 +115,30 @@ function correr(coche){
             }
   },Math.floor((Math.random()*20)+10));
 }
-
+//MUESTRA LA CLASIFICACION DE LOS COCHES.
+function mostrarClasificacion(i){
+  //creo p para despues eliminarlos en reiniciar y volver a crearlos en la opcion de mostrarClasificacion de la partida.
+  var clasis = document.createElement("p");
+  clasis.id="clasificacion";
+  document.getElementById("tabla").appendChild(clasis);
+  numeroCoche = document.getElementById("nCoche");
+  //console.log(i)
+  // si quieres imprimir text en vez de img = document.getElementById("clasificacionP").innerHTML
+  document.getElementById("clasificacion").innerHTML += "<h3>" + clasificado + ". <img src=\"" + document.getElementById(i).src
+  + "\" class=\"cochecito\"></h3>";
+  if (clasificado == 6) {
+    document.getElementById("tabla").style.visibility="visible";
+    if (dinero == 0) {
+      document.getElementById("otra").style.visibility="hidden"
+      document.getElementById("reset").style.visibility="visible";
+    }else {
+      document.getElementById("otra").style.visibility="visible";
+      document.getElementById("reset").style.visibility="visible";
+    }
+  }
+}
+//MUESTRA EL RESULTADO DE LA APUESTA.
 function resultado(){
-
   if (dinero >= parseInt(document.getElementById("apuesta1").value) || dinero == 0) {
     if (cocheApostado == clasificacion[0] && document.getElementById("apuesta1").value != 0) {
         //Ha ganado la apuesta
@@ -107,48 +153,7 @@ function resultado(){
         }
   }
 }
-function mostrarClasificacion(i){
-  //creo p para despues eliminarlos en reiniciar y volver a crearlos en la opcion de mostrarClasificacion de la partida.
-  var clasis = document.createElement("p");
-  clasis.id="clasificacion";
-  document.getElementById("tabla").appendChild(clasis);
-  numeroCoche = document.getElementById("nCoche");
-  //console.log(i)
-  // si quieres imprimir text en vez de img = document.getElementById("clasificacionP").innerHTML
-  document.getElementById("clasificacion").innerHTML += "<h3>" + clasificado + ". <img src=\"" + document.getElementById(i).src
-  + "\" class=\"cochecito\"></h3>";
-
-  if (clasificado == 6) {
-    document.getElementById("tabla").style.visibility="visible";
-    if (dinero == 0) {
-      document.getElementById("otra").style.visibility="hidden"
-      document.getElementById("reset").style.visibility="visible";
-    }else {
-      document.getElementById("otra").style.visibility="visible";
-      document.getElementById("reset").style.visibility="visible";
-    }
-  }
-}
-function cochesEnApuesta() {
-    document.getElementById("coches").innerHTML = "";
-    for(var i = 1; i <= 6; i++) {
-        document.getElementById("coches").innerHTML += "<option value=\"" + i + "\">" + i + "</option>";
-    }
-}
-function apuesta(){
-  document.getElementById("go").disabled = false;
-  document.getElementById("apuesta").style.visibility="hidden";
-  if (dinero >= parseInt(document.getElementById("apuesta1").value)) {
-      dinero -= parseInt(document.getElementById("apuesta1").value);
-      cocheApostado = "coche" + document.getElementById("coches").value;
-  }else if (dinero <= parseInt(document.getElementById("apuesta1").value)) {
-      document.getElementById("dinero").innerHTML = "<strong>NO PUEDES APOSTAR MAS QUE TU SALDO:" + dinero
-      + "€ Y HAS APOSTADO: " + document.getElementById("apuesta1").value + "€</strong>";
-  }else {
-      document.getElementById("dinero").innerHTML = "<strong>SALDO: </strong> " + dinero + "€";
-  }
-}
-//Para seguir jugando.
+//PARA SEGUIR JUGANDO.
 function reiniciar() {
     document.getElementById("otra").style.visibility="hidden";
     document.getElementById("reset").style.visibility="hidden";
@@ -162,18 +167,18 @@ function reiniciar() {
     var clas = document.getElementById("clasificacion");
     clas.parentNode.removeChild(clas);
     document.getElementById("tabla").style.visibility="hidden";
-    //document.getElementById("otra").style.visibility="hidden";
     document.getElementById("start").style.visibility="visible";
     clasificacion =[];
     clasificado = 0;
     posicion= 0;
     numero = 1;
 }
-
+//RESETEAR LA PARTIDA INCLUYENDO RESETEAR EL DINERO.
 function resetear() {
     dinero = 500;
     reiniciar();
 }
+//GENERA NUMERO ALEATORIOS.
 function random(min, max){
   return Math.floor((Math.random()*max)+min);
 }
